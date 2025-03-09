990a28d2933069060a57c57e4982a87b2e007f6f
Phyo Thura Win <phyothura.lonewolf@gmail.com>
2025-03-09 18:58:34 +0630

    web.php

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-#Route::get('/', function () {
-#    return view('/index');
-#});
-
-Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+Route::get('/', [ProductController::class, 'index'])->name('product.index');
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
 Route::post('/product', [ProductController::class, 'store'])->name('product.store');
 Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
